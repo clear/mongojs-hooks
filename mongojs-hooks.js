0636f9f09f94f166e62c572518688a3a1538b28f
David N. Johnson <me@dnj.io>
2014-02-15 16:59:37 +1100

    Adding tenant() helper to the database object to abstract away multi-tenancy.

--- mongojs-hooks.js
+++ mongojs-hooks.js
@@ -10,4 +10,16 @@ mongojs.db = function (key, dbConnectionString) {
 	return dbConns[key];
 };
 
+//Tenancy helper
+mongojs.Database.prototype.tenant = function () {
+	var tenant = mongojs.Database.prototype.collection.apply(this, arguments);
+	
+	tenant.collection = function (name) {
+		//'this' currently refers to the collection returned as 'tenant' and not the db
+		return mongojs.Database.prototype.collection.call(this, name);
+	};
+
+	return tenant;
+};
+
 module.exports = mongojs;
--- test/db.js
+++ test/db.js
@@ -2,27 +2,31 @@ require("should");
 var mongo = require("../mongojs-hooks");
 
 describe("db", function () {
+	beforeEach(function () {
+		mongo.db("test key", undefined);
+	});
+
+	afterEach(function () {
+		mongo.db("test key", undefined);
+	});
+
 	it("db() - with key and no existing connection  - should return undefined", function () {
-		var db = mongo.db("test");
+		var db = mongo.db("test key");
 		(db === undefined).should.be.ok;
 	});
 
 	it("db() - with key and name - should return a new database connection", function () {
-		var db = mongo.db("test", "mongojs-hooks-tdd");
+		var db = mongo.db("test key", "mongojs-hooks-tdd");
 		db.should.be.ok;
 		db._name.should.equal("mongojs-hooks-tdd");
-
-		mongo.db("test", undefined);
 	});
 
 	it("db() - with key and an existing connection  - should return the database connection", function () {
-		var dbFirst = mongo.db("test", "mongojs-hooks-tdd");
+		var dbFirst = mongo.db("test key", "mongojs-hooks-tdd");
 		
-		var db = mongo.db("test");
+		var db = mongo.db("test key");
 		db.should.be.ok;
 		db._name.should.equal("mongojs-hooks-tdd");
 		db.should.equal(dbFirst);
-
-		mongo.db("test", undefined);
 	});
 });
