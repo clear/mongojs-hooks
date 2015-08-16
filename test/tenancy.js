e71fe6adff649da534666127339cdb2de4577199
David N. Johnson <me@dnj.io>
2015-08-16 13:35:27 +1000

    maint(package): Updates format of test descriptions

--- test/tenancy.js
+++ test/tenancy.js
@@ -9,28 +9,28 @@ describe("tenancy", function () {
 		db = mongo.db("test");
 	});
 
-	it("collection() - with collection name - should return a collection with supplied name", function () {
+	it("collection() with collection name should return a collection with supplied name", function () {
 		var collection = db.collection("tests");
 		collection.should.be.ok;
 		collection._name.should.equal(config.db + ".tests");
 	});
 
-	it("tenant() - with tenant name - should return a collection with supplied name", function () {
+	it("tenant() with tenant name should return a collection with supplied name", function () {
 		var collection = db.tenant("tenant");
 		collection.should.be.ok;
 		collection._name.should.equal(config.db + ".tenant");
 	});
 
-	it("tenant().collection() - with tenant name - should prepend collection name with tenant name", function () {
+	it("tenant().collection() with tenant name should prepend collection name with tenant name", function () {
 		var collection = db.tenant("tenant").collection("tests");
 		collection.should.be.ok;
 		collection._name.should.equal(config.db + ".tenant.tests");
 	});
 
-	//This test doesn't need to assert anything, because if we mock out the call and test to
-	//see if it executes, then we don't evaluate the lazy loading and won't know if it called
-	//correctly. Instead, this test will fail by throwing an exception.
-	it("tenant().collection().find() - with tenant name - should not throw an exception", function (done) {
+	// This test doesn't need to assert anything, because if we mock out the call and test to
+	// see if it executes, then we don't evaluate the lazy loading and won't know if it called
+	// correctly. Instead, this test will fail by throwing an exception.
+	it("tenant().collection().find() with tenant name should not throw an exception", function (done) {
 		var collection = db.tenant("tenant").collection("tests");
 
 		collection.find(function () {
